78d585dbd734095d7ef97973f0a351204e2e2960
moryzulu <morymacho6@gmail.com>
2022-08-09 17:25:49 -0700

    print interger file inclusion

--- functions.c
+++ functions.c
@@ -35,68 +35,11 @@ void _print_char_ptr(char *specifier, va_list args)
 			j++;
 		}
 	}
-	/*if (*specifier == 'p')
-	{
-		char* a;
-		void* pVoid = va_arg(args, char*);
-		a = (char*)pVoid;
-		while(a[i])
-		{
-			_putchar(a[i]);
-		}
-	}*/
-}
-/**
- * _print_int - function to print integer
- * @args: valist argument
- * @specifier: ponter specifier for integer
- *
- * Return: an integer type
- */
-void _print_int(char *specifier, va_list args)
-{
-	if(*specifier == 'd')
-	{
-		int n = (int) va_arg(args, int);
-		print_number(n);
-	}
-	if(*specifier == 'i')
-	{
-		int n = (int) va_arg(args, int);
-		print_number(n);
-	}
-	if(*specifier == 'u')
-	{
-		int n = (unsigned int)va_arg(args, int);
-		if (n < 0)
-		{
-			n*= - 1;
-		}
-		print_number(n);
-	}
-	if(*specifier == 'o')
-	{
-		int oct = 0;
-		int mul = 1;
 
-		int n = (unsigned int)va_arg(args, int);
-		if (n < 0)
-		{
-			n*= - 1;
-		}
-		while (n != 0)
-		{
-			oct = (n % 8) * mul;
-			mul = mul * 10;
-			n = n / 8;
-		}
-		print_number(oct);
-	}
 }
 /**
  * print_number - function to print number
  * @n: integer argument
- *
  * Return: number
  */
 void print_number(int n)
---
+++ print_int.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * _print_int - function to print integer
+ * @args: valist argument
+ * @specifier: ponter specifier for integer
+ *
+ * Return: an integer type
+ */
+void _print_int(char *specifier, va_list args)
+{
+	if (*specifier == 'd')
+	{
+		int n = (int) va_arg(args, int);
+		print_number(n);
+	}
+	if (*specifier == 'i')
+	{
+		int n = (int) va_arg(args, int);
+		print_number(n);
+	}
+	if (*specifier == 'u')
+	{
+		int n = (unsigned int)va_arg(args, int);
+		if (n < 0)
+		{
+			n *= - 1;
+		}
+		print_number(n);
+	}
+	if (*specifier == 'o')
+	{
+		int oct = 0;
+		int mul = 1;
+
+		int n = (unsigned int)va_arg(args, int);
+		if (n < 0)
+		{
+			n *= - 1;
+		}
+		while (n != 0)
+		{
+			oct = (n % 8) * mul;
+			mul = mul * 10;
+			n = n / 8;
+		}
+		print_number(oct);
+	}
